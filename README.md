b753e784547b392acd6608f8a2df244ace875149
Jorman Espinoza <jespinoza@glamit.com.ar>
2023-10-31 21:13:29 -0300

    build: Apply initial project structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# daza-proper
+# Proper
+## Landing | DAZA
---
+++ src/scripts/js/main.js
@@ -0,0 +1,7 @@
+const developer = {
+  first_name: 'Jorman',
+  last_name: 'Espinoza',
+  email: 'espinoza.dev@gmail.com'
+}
+
+console.info({ developer })
