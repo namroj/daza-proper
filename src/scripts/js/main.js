fd62fff56120a4a187f072cb81861c1973bcdf97
Jorman Espinoza <espinoza.dev@gmail.com>
2023-11-20 14:13:04 -0300

    feature: Add mobile menu

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -4,8 +4,48 @@ import './backstretch.min.js'
 import './jquery.bxslider.js'
 import './wow.min.js'
 import './scrollTo.min.js'
+import './validate.js'
 
+let screenWidth = window.innerWidth > 0 ? window.innerWidth : screen.width
+let headerHeight
+const sm = 767
 const sliders = []
+
+const onScroll = () => {
+  headerHeight = $('header').height()
+
+  if (screenWidth <= sm) {
+    $(window).scrollTop() > headerHeight
+      ? $('nav:not(.visible) .toggler').addClass('scrolled')
+      : $('.toggler').removeClass('scrolled')
+  }
+}
+
+const buildMenu = () => {
+  $('.toggler').on('click', function () {
+    $('nav').toggleClass('visible')
+    $(this).toggleClass('clicked')
+
+    if ($('nav').hasClass('visible')) {
+      $(this).removeClass('scrolled')
+    }
+
+    if (!$(this).hasClass('clicked')) {
+      if (screenWidth <= sm) {
+        $(window).scrollTop() > headerHeight
+          ? $('.toggler').addClass('scrolled')
+          : $('.toggler').removeClass('scrolled')
+      }
+    }
+  })
+
+  $('nav li a').on('click', function () {
+    if (screenWidth <= sm) {
+      $('nav').toggleClass('visible')
+      $('.toggler').toggleClass('clicked')
+    }
+  })
+}
 
 const updateSliderCaption = (index, container, device) => {
   const caption = $(`#unidades .bxslider.${device} li:not(.bx-clone)`)
@@ -70,19 +110,108 @@ const generateSliders = () => {
   sliders.push(headerSmSlider, headerMdSlider, unitsSmSlider, unitsMdSlider)
 }
 
-const getCurrentYear = () => new Date().getFullYear()
+const buildForm = () => {
+  $('form').validate({
+    rules: {
+      first_name: {
+        required: true
+      },
+      last_name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      phone: {
+        required: true
+      },
+      query: {
+        required: true
+      }
+    },
+    errorClass: 'error',
+    errorPlacement: function (error, element) {
+      $(element).prev().children().text(error[0].innerText)
+    },
+    success: function (label, element) {
+      $(element).prev().children().text('')
+    },
+    submitHandler: function (form) {
+      $('form button').text('Enviando...')
+      const firstName = $('form [name="first_name"]').val()
+      const lastName = $('form [name="last_name"]').val()
+      const email = $('form [name="email"]').val()
+      const phone = $('form [name="phone"]').val()
+      const query = $('form [name="query"]').val()
+
+      const data = `first_name=${firstName}&last_name=${lastName}&email=${email}&phone=${phone}$query=${query}`
+
+      $.ajax({
+        type: 'POST',
+        url: `http://127.0.0.1:5500/mailing`,
+        data: data,
+        dataType: 'json',
+        success: function (response) {
+          if (response.status != 'OK') {
+            $('.response').html(response.msg).show().animate(
+              {
+                opacity: 1
+              },
+              500
+            )
+            $('form button').text('Enviar')
+          } else {
+            $('form')[0].reset()
+            $('form button').text('Enviar')
+            $('.response').html(response.msg).animate(
+              {
+                opacity: '1'
+              },
+              500
+            )
+            setTimeout(function () {
+              $('.response').html(response.msg).css('opacity', 0)
+            }, 4000)
+          }
+
+          setTimeout(function () {
+            $('form button').text('Enviar')
+            $('.response').css('opacity', 0)
+          }, 4000)
+        }
+      })
+
+      return false
+    }
+  })
+}
+
+const setCurrentYear = () => {
+  $('#current-year').text(new Date().getFullYear())
+}
 
 $(function () {
-  $('#current-year').text(getCurrentYear())
+  $(document).on('scroll', onScroll)
+  buildMenu()
+  buildForm()
+  setCurrentYear()
 })
 
 $(window).on('resize', () => {
+  setTimeout(function () {
+    screenWidth = window.innerWidth > 0 ? window.innerWidth : screen.width
+    if (screenWidth <= sm) {
+      $('nav').removeClass('scrolled')
+    }
+  }, 500)
+
   sliders.forEach((slider) => slider.reloadSlider())
 })
 
 $(window).on('load', () => {
   $('#loader').delay(1000).fadeOut('slow')
-
+  onScroll()
   generateSliders()
 })
 
